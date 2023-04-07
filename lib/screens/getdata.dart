032b3520ad040afd56bd53636ca45a748903b013
ddrechse <doug.drechsel@oracle.com>
2023-04-07 09:44:57 -0400

    Implement Get and Put screens

--- lib/screens/getdata.dart
+++ lib/screens/getdata.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 
 class GetData extends StatefulWidget {
   const GetData({Key? key}) : super(key: key);
@@ -8,32 +9,146 @@ class GetData extends StatefulWidget {
 }
 
 class _GetDataState extends State<GetData> {
+  late Future<List<ParseObject>> futureData;
+
+  @override
+  void initState() {
+    super.initState();
+    futureData = fetchData();
+  }
+
+  Future<List<ParseObject>> fetchData() async {
+    var apiResponse = await ParseObject('GameScore').getAll();
+
+    if (apiResponse.success) {
+      if (apiResponse.result != null) {
+        return apiResponse.result;
+      } else {
+        return [];
+      }
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to retrieve GameScores');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Get Gamescores")),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          child: ListView(
-            children: const [
-              Text(
-                'Get Gamescores goes here',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w800,
-                  fontFamily: 'Roboto',
-                  letterSpacing: 0.5,
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-            ],
-          ),
-        ),
+    Widget scoressSection = Container(
+      child: Column(
+        children: [
+          FutureBuilder<List<ParseObject>>(
+            future: futureData,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                print(snapshot.data);
+                print(snapshot.data!.length);
+                //               for (var testObject in (snapshot.data)!) {
+                //                 print(testObject.toString());
+                //                 print(testObject['score']);
+                //               }
+
+                List<ParseObject>? data = snapshot.data;
+                print(data);
+                return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  physics: const ClampingScrollPhysics(),
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          ListTile(
+                            leading: const Icon(Icons.scoreboard),
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  "Player Name:",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(data![index]['playerName']),
+                                const Text(
+                                  "Score:",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  data[index]['score'].toString(),
+                                ),
+                              ],
+                            ),
+                            subtitle: Text(
+                                "CheatMode Enabled:  ${data[index]['cheatmode']}"),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          )
+        ],
       ),
     );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Gamescore'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                const Text(
+                  'Gamescores',
+                  textScaleFactor: 2,
+                ),
+                const SizedBox(height: 20),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    scoressSection,
+                  ],
+                ),
+                Container(
+                    height: 50,
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: ElevatedButton(
+                      child: const Text('Refresh'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const GetData(),
+                          ),
+                        );
+                      },
+                    )),
+              ],
+            )));
+
+/*        return Wrap(
+      children: <Widget>[
+        const Text(
+          'Gamescores',
+          textScaleFactor: 2,
+        ),
+        const SizedBox(height: 20),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            scoressSection,
+          ],
+        ),
+      ],
+    );*/
   }
 }
--- lib/screens/putdata.dart
+++ lib/screens/putdata.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 
 class PutData extends StatefulWidget {
   const PutData({Key? key}) : super(key: key);
@@ -8,32 +9,113 @@ class PutData extends StatefulWidget {
 }
 
 class _PutDataState extends State<PutData> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController scoreController = TextEditingController();
+  bool? check1 = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Enter Gamescores")),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          child: ListView(
-            children: const [
-              Text(
-                'Enter Gamescores goes here',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w800,
-                  fontFamily: 'Roboto',
-                  letterSpacing: 0.5,
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text('Gamescore'),
         ),
-      ),
-    );
+        body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'Enter Gamescore',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Player Name',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    controller: scoreController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Score',
+                    ),
+                  ),
+                ),
+                Container(
+                  child: CheckboxListTile(
+                    value: check1,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        check1 = value;
+                      });
+                    },
+                    title: Text("CheatMode Enabled?"),
+                  ),
+                ),
+                Container(
+                    height: 75,
+                    padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+                    child: ElevatedButton(
+                      child: const Text('Enter'),
+                      onPressed: () {
+                        print("Controller name = ${nameController.text}");
+                        print("Controller score = ${scoreController.text}");
+                        print("Cheat Enabled = $check1");
+                        enterGameScore(context, nameController.text,
+                            scoreController.text, check1);
+                        scoreController.clear();
+                        nameController.clear();
+                      },
+                    )),
+              ],
+            )));
+  }
+
+  enterGameScore(context, playerName, score, cheatModeEnabled) async {
+    var scoreNum = int.parse(score);
+    // Create a new Gamescore document
+    var gamescoreObject = ParseObject('GameScore')
+      ..set('playerName', playerName)
+      ..set('score', scoreNum)
+      ..set('cheatmode', cheatModeEnabled);
+    // And save it
+    var response = await gamescoreObject.save();
+    if (response.success) {
+      gamescoreObject = response.results?.first;
+    } else {
+      // set up the button
+      Widget okButton = TextButton(
+        child: const Text("OK"),
+        onPressed: () => Navigator.of(context, rootNavigator: true).pop(),
+      );
+      // set up the AlertDialog
+      AlertDialog alert = AlertDialog(
+        title: const Text("Error Dialog"),
+        content: Text('${response.error?.message}'),
+        actions: [
+          okButton,
+        ],
+      );
+
+      // show the dialog
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        },
+      );
+    }
   }
 }
